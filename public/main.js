fb4d8fae1191b9b057a9a4ef4f379670dce6a9e8
Yuto <218c0443@s.obirin.ac.jp>
2021-07-08 18:20:34 +0900

    Javaは別ファイルに分けた

---
+++ public/main.js
@@ -0,0 +1,16 @@
+const socketio = io();
+const form = document.getElementById("form");
+const input = document.getElemenyById("msg");
+const chats = document.getElementById("chats");
+
+form.addEventListener('submit', function(event){
+   socketio.emit("message", input.value);
+   input.value = '';
+   event.preventDefault();
+})
+
+socketio.on('message', function(msg){
+   const li = document.createElement("li");
+   li.append(msg);
+   chats.append(li);
+})
